6daaead266cae81889d73bc1c5c81c50fef7c218
IsleBeeGun <IsleBeeGun@gmail.com>
2020-08-09 23:29:38 +0300

    finished constructing clinicsList.json5

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,37 @@
 // DO NOT REMOVE NEXT LINE
 require("../less/index.less");
 
-// Loading lists of clinics
-import clinicsList from '../clinicsList.json5';
+// Handler for selecting clinics
+import SlimSelect from "slim-select";
 
-console.log(clinicsList.clinics[0].text);
+// Loading lists of clinics
+import clinicsList from "../clinicsList.json5";
+
+// Applying clinics to clinics-selector
+// clinicsList.clinics.forEach( clinic => {
+//     const option = document.createElement("option");
+//     option.innerText = clinic.name;
+//     option.value = clinic.name;
+//     document.getElementById("clinics-selector").appendChild(option);
+// });
+
+// Applying & configuring SlimSelect
+var select = new SlimSelect({
+  select: "#clinics-selector",
+  placeholder: "Выберите медцентр",
+  searchPlaceholder: "Поиск",
+  searchText: 'Нет результатов',
+  searchFocus: false,
+  showContent: "down",
+  data: clinicsList.clinics.map((clinic) => {
+    return { text: clinic.name, value: clinic.name };
+  }),
+  onChange: (option) => {
+    console.log(option);
+    document.getElementById(
+      "clinics-result"
+    ).innerHTML = clinicsList.clinics.find(
+      (clinic) => clinic.name == option.value
+    ).text;
+  },
+});
